3b0543a15e6a1ccfc9c329e8505cbecfb063caf9
AnnaStandridge <annanstandridge@gmail.com>
2023-09-26 18:22:08 -0400

    Project 12

---
+++ app.js
@@ -0,0 +1,29 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const { PORT = 3001 } = process.env;
+
+const app = express();
+
+mongoose.connect(
+  "mongodb://127.0.0.1:27017/wtwr_db",
+  (r) => {
+    console.log("connected to db", r);
+  },
+  (e) => console.log("db error", e),
+);
+
+const routes = require("./routes/index");
+
+app.use(express.json());
+app.use(routes);
+
+app.use((req, res, next) => {
+  req.user = {
+    _id: "650246c6173eca7186f65581",
+  };
+  next();
+});
+
+app.listen(PORT, () => {
+  console.log(`App listening at port ${PORT}`);
+});
---
+++ controllers/clothingitem.js
@@ -0,0 +1,79 @@
+const res = require("express/lib/response");
+const ClothingItem = require("../models/clothingItem");
+
+const createItem = (req, res) => {
+  console.log(req);
+  console.log(req.body);
+
+  const { name, weather, imageURL } = req.body;
+  const owner = req.user._id;
+
+  ClothingItem.create({ name, weather, imageURL, owner })
+    .then((item) => {
+      console.log(item);
+      res.send({ data: item });
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from createItem", e });
+    });
+};
+
+const getItems = (req, res) => {
+  ClothingItem.find({})
+    .then((items) => res.status(200).send(items))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from getItems", e });
+    });
+};
+
+const updateItem = (req, res) => {
+  const { itemId } = req.param;
+  const { imageURL } = req.body;
+
+  ClothingItem.findByIdAndUpdate(itemId, { $set: { imageURL } })
+    .orFail()
+    .then((item) => res.status(200).send({ data: item }))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from updateItem", e });
+    });
+};
+
+const deleteItem = (req, res) => {
+  const { itemId } = req.params;
+  ClothingItem.findByIdAndDelete(itemId)
+    .orFail()
+    .then((item) => res.status(204).send({}))
+    .catch((e) => {
+      res.status(500).send({ message: "Error from updateItem", e }),
+    });
+};
+
+const likeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $addToSet: { likes: req.user._id } },
+    { new: true })
+    .orFail()
+    .then(like => {
+      res.status(200).send(like);
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from likeItem", e }),
+    })
+}
+
+const dislikeItem = (req, res) => {
+  ClothingItem.findByIdAndUpdate(
+    req.params.itemId,
+    { $pull: { likes: req.user._id } },
+    { new: true })
+    .orFail()
+    .then(dislike => {
+      res.status(200).send(dislike);
+    })
+    .catch((e) => {
+      res.status(500).send({ message: "Error from dislikeItem", e }),
+    })
+}
+
+module.exports = { createItem, getItems, updateItem, deleteItem, likeItem, dislikeItem };
